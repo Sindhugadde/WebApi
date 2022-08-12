16bc7f25da54cc4d7185805ff678d6eb6709a3c2
sindhugadde <gaddesindhu98@gmail.com>
2022-08-12 13:56:13 -0400

    Added the Api code

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using WebApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Cors;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+    
+        [JsonConstructor]
+
+        public UsersController( IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        [EnableCors]
+        public Array Get()
+        {
+           string query = @"
+select firstname,lastname,telephone,email from webappDB.dbo.submitusers";
+     
+            DataTable table = new DataTable();
+            string sqldtsource = _configuration.GetConnectionString("usersAppCon");
+            SqlDataReader myreader;
+
+            using (SqlConnection myconn = new SqlConnection(sqldtsource))
+            {
+                myconn.Open();
+                using (SqlCommand mycommand = new SqlCommand(query,myconn))
+                {
+                    myreader = mycommand.ExecuteReader();
+                    table.Load(myreader);
+                    
+                    myreader.Close();
+                    myconn.Close();
+                    
+                }
+
+            }
+            string JSONresult;
+            JSONresult = JsonConvert.SerializeObject(table);
+            submitusers[] users = JsonConvert.DeserializeObject<submitusers[]>(JSONresult);
+            return users;
+        }
+
+              [HttpPost]
+        [Route("AddUser")]
+        [EnableCors]
+
+        public JsonResult Post( submitusers user )
+        {
+            string query = @"insert into dbo.submitusers(firstname,lastname,telephone,email) 
+      values(
+          '" + user.Firstname +@"'
+          ,'"  + user.Lastname+@"'
+          ,'" +user.Telephone+@"' 
+          ,'" +user.Email+@"'
+           )";
+            DataTable table = new DataTable();
+            string sqldtsource = _configuration.GetConnectionString("usersAppCon");
+            SqlDataReader myreader;
+            using (SqlConnection myconn = new SqlConnection(sqldtsource))
+            {
+                myconn.Open();
+                using (SqlCommand mycommand = new SqlCommand(query, myconn))
+                {
+                    myreader = mycommand.ExecuteReader();
+                    table.Load(myreader);
+                    myreader.Close();
+                    myconn.Close();
+
+                }
+            }
+            return new JsonResult("Added the user successfully");
+        }
+    }
+}
---
+++ WebApi/Models/submitusers.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Models
+{
+    public class submitusers
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Telephone { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
